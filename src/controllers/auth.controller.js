961f0d811c50b904bdc3c5899c481cd4a01e7da8
kavin <kavinvenkatesan2008@gmail.com>
2026-06-01 09:37:21 +0530

    solving the issues

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/user.model.js');
+const User = require('../Models/user.model.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
